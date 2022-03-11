9f78ad96b849b6d52c8c38efa9010a2c21fae511
Uhbaabys <mr.verygood@inbox.ru>
2022-03-11 13:43:08 +0700

    first commit

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Swiper.scss';
+
+
+
+const Swiper = () =>{
+    return(
+        <div className={'swiper-container'}>
+            <div className={'swiper-wrapper'}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Swiper;
